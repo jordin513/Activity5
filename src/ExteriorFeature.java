3250515e2b22c3f52e136c3144529a667abca31d
jlmedina8937 <jordin513@gmail.com>
2018-10-05 10:43:03 -0400

    up to step 7 (2)

---
+++ src/ExteriorFeature.java
@@ -0,0 +1,23 @@
+public class ExteriorFeature implements Feature {
+
+  String exteriorFeature;
+
+  public ExteriorFeature() {
+    this.exteriorFeature = exteriorFeature;
+  }
+
+  public ExteriorFeature(String exteriorFeature) {
+
+    this.exteriorFeature = exteriorFeature;
+  }
+
+  @Override
+  public String getFeature() {
+    return null;
+  }
+
+  @Override
+  public void setFeature(String feature) {
+
+  }
+}
